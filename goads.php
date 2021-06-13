9ff7107a1ba2ec33b83cea5da2f3a4a2ec7d9ac3
MyTarget-org <85835387+MyTarget-org@users.noreply.github.com>
2021-06-13 17:58:58 +0300

    Сайт MyTarget.org (Интернет-маркетинг)
    Добавили сайт: https://www.mytarget.org/ в проект My Target.

---
+++ goads.php
@@ -0,0 +1,22 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("contactus@mytarget.org", "Заявка с сайта", "ФИО: ".$fio.". E-mail: ".$email ,"From: contactus2@mytarget.org \r\n"))
+ {     echo "Сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
+<!--Переадресация на главную страницу сайта, через 3 секунды-->
+<script language="JavaScript" type="text/javascript">
+function changeurl(){eval(self.location="https://www.mytarget.org/googleads.html");}
+window.setTimeout("changeurl();",3000);
+</script>
